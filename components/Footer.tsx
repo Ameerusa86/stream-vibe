cb271f8f4c06880bcaac009a01e32e6928212098
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-11-08 21:05:02 -0500

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Wrapper from "./Wrapper";
+import { footerLinks } from "@/constants/FooterLinks";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full h-full bg-black-06">
+      {/* Footer Links */}
+      <Wrapper>
+        <div className="flex items-start justify-between gap-8">
+          {footerLinks.map((link, index) => (
+            <div className="flex flex-col gap-8" key={index}>
+              <h1 className="text-lg font-semibold text-white">{link.title}</h1>
+              <ul className="gap-3 flex flex-col">
+                {link.links.map((item, index) => (
+                  <li key={index}>
+                    <Link
+                      href={item.url}
+                      className="text-white hover:text-primary-500"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </Wrapper>
+      {/* Copyrights */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ constants/FooterLinks.ts
@@ -0,0 +1,104 @@
+export const footerLinks = [
+  {
+    title: "Home",
+    links: [
+      {
+        name: "Categories",
+        url: "/",
+      },
+      {
+        name: "Devices",
+        url: "/",
+      },
+      {
+        name: "Pricing",
+        url: "/",
+      },
+      {
+        name: "FAQ",
+        url: "/",
+      },
+    ],
+  },
+  {
+    title: "Movies",
+    links: [
+      {
+        name: "Action",
+        url: "/",
+      },
+      {
+        name: "Adventure",
+        url: "/",
+      },
+      {
+        name: "Comedy",
+        url: "/",
+      },
+      {
+        name: "Drama",
+        url: "/",
+      },
+    ],
+  },
+  {
+    title: "TV Shows",
+    links: [
+      {
+        name: "Action",
+        url: "/",
+      },
+      {
+        name: "Adventure",
+        url: "/",
+      },
+      {
+        name: "Comedy",
+        url: "/",
+      },
+      {
+        name: "Drama",
+        url: "/",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      {
+        name: "Help Center",
+        url: "/",
+      },
+      {
+        name: "Contact Us",
+        url: "/",
+      },
+    ],
+  },
+  {
+    title: "Subscriptions",
+    links: [
+      {
+        name: "Plans",
+        url: "/",
+      },
+      {
+        name: "Features",
+        url: "/",
+      },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/Ameerusa86",
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/ameer86/",
+      },
+    ],
+  },
+];
