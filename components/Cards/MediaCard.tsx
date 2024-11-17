2d2289843fe92f3c719279b82e7ba879c214f243
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-11-17 11:14:36 -0500

    update MediaCard component to adjust link paths for movies and TV shows

--- components/Cards/MediaCard.tsx
+++ components/Cards/MediaCard.tsx
@@ -19,7 +19,10 @@ const MediaCard: React.FC<MediaCardProps> = ({ item, type, slug }) => {
   const rating = item.vote_average ? item.vote_average.toFixed(1) : "N/A";
 
   return (
-    <Link href={`/${type}/${slug}`} className="group">
+    <Link
+      href={`/${type === "movie" ? "movies" : "tvshows"}/${type}/${item.id}`}
+      className="group"
+    >
       <div className="bg-black-10 p-4 rounded-lg shadow-lg w-[300px] h-[400px] md:w-[350px] md:h-[400px] flex flex-col justify-between">
         <div className="relative w-full h-[300px] md:h-[340px] mb-2 overflow-hidden rounded-md">
           <Image
