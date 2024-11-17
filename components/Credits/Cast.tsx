878746d40e368b01a777f4f663295f70904b4ea7
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-11-17 10:52:55 -0500

    refactor Cast component to simplify image rendering and improve fallback logic

--- components/Credits/Cast.tsx
+++ components/Credits/Cast.tsx
@@ -6,53 +6,35 @@ interface CastProps {
   name: string;
   character: string;
   CastImage?: string;
-  gender?: number;
+  gender?: number; // 1 for female, 2 for male
 }
 
 const Cast = ({ name, character, CastImage, gender }: CastProps) => {
   const imgUrl = "https://image.tmdb.org/t/p/original";
 
+  // Determine the fallback image based on gender
+  const fallbackImage =
+    gender === 2
+      ? images.maleImg // Male fallback image
+      : images.femaleImg; // Female fallback image
+
   return (
     <div className="shadow-lg rounded-lg">
-      {gender === 0 ? (
-        CastImage ? (
-          <Image
-            src={imgUrl + CastImage}
-            width={500}
-            height={500}
-            alt="cast"
-            className="rounded-lg w-[110px] h-[110px] object-cover"
-          />
-        ) : (
-          <Image
-            src={images.maleImg}
-            width={500}
-            height={500}
-            alt="cast"
-            className="rounded-lg w-[110px] h-[110px] object-cover"
-          />
-        )
-      ) : CastImage ? (
-        <Image
-          src={imgUrl + CastImage}
-          width={500}
-          height={500}
-          alt="cast"
-          className="rounded-lg w-[110px] h-[110px] object-cover"
-        />
-      ) : (
-        <Image
-          src={images.femaleImg}
-          width={500}
-          height={500}
-          alt="cast"
-          className="rounded-lg w-[110px] h-[110px] object-cover"
-        />
-      )}
+      {/* Render Cast Image or Fallback */}
+      <Image
+        src={CastImage ? `${imgUrl}${CastImage}` : fallbackImage}
+        width={500}
+        height={500}
+        alt={`${name} image`}
+        className="rounded-lg w-[110px] h-[110px] object-cover"
+      />
 
+      {/* Cast Name and Character */}
       <div>
-        <p className="text-center mt-3 font-bold">{name}</p>
-        <p className="text-center mt-1 text-base">{character}</p>
+        <p className="text-center mt-3 font-bold truncate">{name}</p>
+        <p className="text-center mt-1 text-base text-gray-600 truncate">
+          {character}
+        </p>
       </div>
     </div>
   );
